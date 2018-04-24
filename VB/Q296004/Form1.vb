cc3611cda99c478646d6509e35ed0077c4d81ec4
DevExpressExampleBot <fakeEmail>
2018-04-24 19:41:53 +0300

    Source auto update [skip ci]

--- VB/Q296004/Form1.vb
+++ VB/Q296004/Form1.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports DXSample
 Imports DevExpress.Xpo
 Imports DevExpress.XtraGrid
@@ -11,42 +10,43 @@ Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.XtraEditors.Controls
 
 Namespace Q296004
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class Form1
+        Inherits Form
 
-		Private Sub gridView1_ValidatingEditor(ByVal sender As Object, ByVal e As BaseContainerValidateEditorEventArgs) Handles gridView1.ValidatingEditor
-			Dim view As GridView = CType(sender, GridView)
-			Dim row As XPBaseObject = CType(view.GetFocusedRow(), XPBaseObject)
-			Dim customValues As New Dictionary(Of String, Object)()
-			customValues.Add(view.FocusedColumn.FieldName, e.Value)
-			e.ErrorText = New Validator(row, customValues).Validate(view.FocusedColumn.FieldName)
-			e.Valid = String.IsNullOrEmpty(e.ErrorText)
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub gridView1_ValidateRow(ByVal sender As Object, ByVal e As ValidateRowEventArgs) Handles gridView1.ValidateRow
-			If e.RowHandle <> GridControl.NewItemRowHandle Then
-				Return
-			End If
-			Dim customValues As New Dictionary(Of String, Object)()
-			Dim view As GridView = CType(sender, GridView)
-			For Each col As GridColumn In view.Columns
-				customValues.Add(col.FieldName, view.GetRowCellValue(e.RowHandle, col))
-			Next col
-			Dim validator As New Validator(CType(e.Row, XPBaseObject), customValues)
-			For Each col As GridColumn In (CType(sender, GridView)).Columns
-				e.ErrorText = validator.Validate(col.FieldName)
-				If (Not String.IsNullOrEmpty(e.ErrorText)) Then
-					e.Valid = False
-					Return
-				End If
-			Next col
-		End Sub
+        Private Sub gridView1_ValidatingEditor(ByVal sender As Object, ByVal e As BaseContainerValidateEditorEventArgs) Handles gridView1.ValidatingEditor
+            Dim view As GridView = DirectCast(sender, GridView)
+            Dim row As XPBaseObject = CType(view.GetFocusedRow(), XPBaseObject)
+            Dim customValues As New Dictionary(Of String, Object)()
+            customValues.Add(view.FocusedColumn.FieldName, e.Value)
+            e.ErrorText = (New Validator(row, customValues)).Validate(view.FocusedColumn.FieldName)
+            e.Valid = String.IsNullOrEmpty(e.ErrorText)
+        End Sub
 
-		Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles MyBase.FormClosing
-			unitOfWork1.CommitChanges()
-		End Sub
-	End Class
+        Private Sub gridView1_ValidateRow(ByVal sender As Object, ByVal e As ValidateRowEventArgs) Handles gridView1.ValidateRow
+            If e.RowHandle <> GridControl.NewItemRowHandle Then
+                Return
+            End If
+            Dim customValues As New Dictionary(Of String, Object)()
+            Dim view As GridView = DirectCast(sender, GridView)
+            For Each col As GridColumn In view.Columns
+                customValues.Add(col.FieldName, view.GetRowCellValue(e.RowHandle, col))
+            Next col
+            Dim validator As New Validator(CType(e.Row, XPBaseObject), customValues)
+            For Each col As GridColumn In DirectCast(sender, GridView).Columns
+                e.ErrorText = validator.Validate(col.FieldName)
+                If Not String.IsNullOrEmpty(e.ErrorText) Then
+                    e.Valid = False
+                    Return
+                End If
+            Next col
+        End Sub
+
+        Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
+            unitOfWork1.CommitChanges()
+        End Sub
+    End Class
 End Namespace
--- VB/Q296004/PersistentClasses.vb
+++ VB/Q296004/PersistentClasses.vb
@@ -1,34 +1,33 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports DevExpress.Xpo
+﻿Imports DevExpress.Xpo
 
 Namespace DXSample
-	Public Class Person
-		Inherits XPObject
-		Public Sub New(ByVal session As Session)
-			MyBase.New(session)
-		End Sub
+    Public Class Person
+        Inherits XPObject
 
-		Private fName As String
-		<Custom("Validation", "not(Name is null or Name = '');Unique|Please provide the person name;The person with this name is already exists")> _
-		Public Property Name() As String
-			Get
-				Return fName
-			End Get
-			Set(ByVal value As String)
-				SetPropertyValue("Name", fName, value)
-			End Set
-		End Property
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
 
-		Private fAge As Integer
-		<Custom("Validation", "Age between (20, 40)|The age can be between 20 and 40 years old")> _
-		Public Property Age() As Integer
-			Get
-				Return fAge
-			End Get
-			Set(ByVal value As Integer)
-				SetPropertyValue("Age", fAge, value)
-			End Set
-		End Property
-	End Class
+        Private fName As String
+        <Custom("Validation", "not(Name is null or Name = '');Unique|Please provide the person name;The person with this name is already exists")> _
+        Public Property Name() As String
+            Get
+                Return fName
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("Name", fName, value)
+            End Set
+        End Property
+
+        Private fAge As Integer
+        <Custom("Validation", "Age between (20, 40)|The age can be between 20 and 40 years old")> _
+        Public Property Age() As Integer
+            Get
+                Return fAge
+            End Get
+            Set(ByVal value As Integer)
+                SetPropertyValue("Age", fAge, value)
+            End Set
+        End Property
+    End Class
 End Namespace
